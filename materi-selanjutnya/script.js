3bd6234218b118e82180b154764992355d886df1
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2025-04-28 22:52:30 +0700

    Eps. 27 (End) - Belajar JavaScript OOP : Materi Selanjutnya

---
+++ materi-selanjutnya/script.js
@@ -0,0 +1,6 @@
+// # Materi Selanjutnya
+// JavaScript Standard Library : membahas standar-standar library yang ada di javascript / disediakan oleh javascript
+// Javascript Modules -- rekomendasi materi jika ingin belajar mengenai node js
+// JavaScript Document Object Model
+// JavaScript Async / Asynchronous
+// JavaScript Web API
